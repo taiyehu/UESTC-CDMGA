f700b9ebfe385d47cb72f3f141d029b43388ae56
ETL1224 <626668536@qq.com>
2025-07-10 20:27:58 +0800

    1

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import VueRouter from 'vue-router';
 import router from './router';
+import axios from 'axios';
+import VueAxios from 'vue-axios'
 Vue.config.productionTip = false
 
 Vue.use(ElementUI);
 Vue.use(VueRouter)
+Vue.use(VueAxios,axios)
 new Vue({
   render: h => h(App),
   router
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -55,9 +55,46 @@
     },
     methods: {
       submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            alert("submit!");
+      // 验证表单中的账号密码是否有效，因为在上面rules中定义为了必填 required: true
+      this.$refs[formName].validate((valid) => {
+        // 点击登录后，让登录按钮开始转圈圈（展示加载动画）
+        this.loading = true;
+        // 如果经过校验，账号密码都不为空，则发送请求到后端登录接口
+        if (valid) {
+          let _this = this;
+          // 使用 axios 将登录信息发送到后端
+          this.axios({
+            url: "/api/user/login",               // 请求地址
+            method: "post",                       // 请求方法
+            headers: {                            // 请求头
+              "Content-Type": "application/json",
+            },
+            params: {                             // 请求参数
+              uname: _this.ruleForm.uname,
+              password: _this.ruleForm.password,
+            },
+          }).then((res) => { // 当收到后端的响应时执行该括号内的代码，res 为响应信息，也就是后端返回的信息
+            if (res.data.code === "0") {  // 当响应的编码为 0 时，说明登录成功
+              // 将用户信息存储到sessionStorage中
+              sessionStorage.setItem("userInfo", JSON.stringify(res.data.data));
+              // 跳转页面到首页
+              this.$router.push('/home');
+              // 显示后端响应的成功信息
+              this.$message({
+                message: res.data.msg,
+                type: "success",
+              });
+            } else {  // 当响应的编码不为 0 时，说明登录失败
+              // 显示后端响应的失败信息
+              this.$message({
+                message: res.data.msg,
+                type: "warning",
+              });
+            }
+            // 不管响应成功还是失败，收到后端响应的消息后就不再让登录按钮显示加载动画了
+            _this.loading = false;
+            console.log(res);
+          });
           } else {
             console.log("error submit!!");
             return false;
