a4d5049d27a8dd9084f2f1f3f0d70ef8a7666a95
taiyehu <2446277107@qq.com>
2025-07-15 20:28:05 +0800

    ports

--- vueproject/vue.config.js
+++ vueproject/vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     // 代理服务器可以将路由中的指定前缀转发到指定的后端服务器中
     proxy: {
       '/api': {
-        target: 'http://localhost:8081',
+        target: 'http://localhost:8080',
         ws: true, // 是否启用websockets
         changeOrigin: true,  // 代理时是否更改host
         pathRewrite: {
