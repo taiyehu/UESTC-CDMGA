3047d38a37c5043bdf893d80ac7791461ae3212f
QGlint <QGlint@outlook.com>
2025-08-29 10:04:40 +0800

    add ssh test -1

--- script/restart_mvn_cdmga.sh
+++ script/restart_mvn_cdmga.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 要运行的新 jar 包路径
-JAR_PATH="/home/youruser/app/yourapp.jar"
+JAR_PATH="/var/www/app-jars/webpage-0.0.1-SNAPSHOT.jar"
 
 # 查找占用 8080 端口的进程
 PID=$(lsof -t -i:8080)
@@ -16,5 +16,5 @@ fi
 
 # 启动新的 Java 程序
 echo "正在启动新的程序: $JAR_PATH"
-nohup java -jar $JAR_PATH > app.log 2>&1 &
-echo "新程序已启动，日志输出到 app.log"
+nohup java -jar $JAR_PATH > /var/www/log/web.log 2>&1 &
+echo "新程序已启动，日志输出到 web.log"
--- script/restart_vue_cdmga.sh
+++ script/restart_vue_cdmga.sh
@@ -1,3 +1,4 @@
-sudo rm -rf /var/www/CDMGA-ui
-sudo cp -r ./vueproject/dist /var/www/CDMGA-ui
-sudo systemctl restart nginx
+rm -rf /var/www/CDMGA-ui
+cp -r  /var/www//dist /var/www/CDMGA-ui
+systemctl restart nginx
+rm -rf /var/www//dist
