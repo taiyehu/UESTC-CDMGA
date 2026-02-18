8d84406846d490339739a32cc0111a9f300cdfca
Qglint <qglint@outlook.com>
2026-02-18 19:53:54 +0800

    viteconfig

--- vueproject-v3/vite.config.ts
+++ vueproject-v3/vite.config.ts
@@ -1,7 +1,18 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 8081,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    }
+  }
 })
